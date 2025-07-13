2b70fd91969357eec71362daa6a5f0853431adaa
Garajnik <lexan1699@gmail.com>
2025-07-13 23:20:22 +0700

    Answer v1

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,46 @@
-/**
- * @file Домашка по FP ч. 2
- *
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- *
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- *
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import * as R from 'ramda';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const isValid = R.allPass([
+  R.pipe(R.length, R.gt(R.__, 2)),
+  R.pipe(R.length, R.lt(R.__, 10)),
+  R.pipe(Number, R.gt(R.__, 0)),
+  R.test(/^[0-9.]+$/),
+]);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const roundNum = x => Promise.resolve(Math.round(Number(x)));
+const log = writeLog => x => { writeLog(x); return Promise.resolve(x); };
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const toBinary = num =>
+  api.get('https://api.tech/numbers/base', { number: num, from: 10, to: 2 })
+    .then(R.prop('result'));
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const lengthOf = s => Promise.resolve(String(s).length);
+const square = x => Promise.resolve(x * x);
+const mod3 = x => Promise.resolve(x % 3);
+const getAnimal = id =>
+  api.get(`https://animals.tech/${id}`)
+    .then(R.prop('result'));
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  writeLog(value);
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  if (!isValid(value)) {
+    handleError('Ошибка валидации');
+    return;
+  }
+
+  roundNum(value)
+    .then(log(writeLog))
+    .then(toBinary).then(log(writeLog))
+    .then(lengthOf).then(log(writeLog))
+    .then(square).then(log(writeLog))
+    .then(mod3).then(log(writeLog))
+    .then(getAnimal)
+    .then(handleSuccess)
+    .catch(() => handleError('Ошибка валидации'));
+};
 
 export default processSequence;
